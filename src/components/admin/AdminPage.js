5e410c9d5825862dce93927ad9cacb443058ffa1
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2022-06-11 00:32:50 +0000

    modification de la partie header pour integrer les différents logos

--- src/components/admin/AdminPage.js
+++ src/components/admin/AdminPage.js
@@ -6,11 +6,31 @@ import { Route, Routes } from 'react-router-dom';
 import DashboardAdmin from './DashboardAdmin';
 import ListRetards from './ListRetards';
 import ListAbsences from './ListAbsences';
+import logo from "../../assets/images/logo.png";
+import flag from "../../assets/images/flag.png";
+import userStatus from "../../assets/images/Ellipse_mercedes.png";
+import "./adminPage.css";
 
 const AdminPage = () => {
     return (
         <div>
-        <h1 className='bg-light shadow'>Navbar</h1>
+        <h1 className='shadow d-flex flex-column flex-md-row justify-content-md-between align-items-center'>
+            <div className="logo align-self-center ms-md-5">
+                <img src={logo} alt="Point me Logo" />
+            </div>
+            <div className="flag d-none d-md-block">
+                <img src={flag} alt="Point me flag" />
+            </div>
+            <div className="userStatus d-none d-md-flex me-md-5">
+                <div className="ellipse-mercedes-img">
+                    <img src={userStatus} alt="Point me user status" />
+                </div>
+                <div className="status">
+                    <p>Ndiaga SALL</p>
+                    <p>Admin</p>
+                </div>
+            </div>
+        </h1>
             <div className="row p-0 m-0">
                 <div className="col-2 m-0 p-0">
                     <SideBarWeb sideBarData={SideBarDataAdmin}/>
